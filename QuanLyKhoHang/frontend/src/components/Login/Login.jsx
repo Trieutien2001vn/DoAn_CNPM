6cda0593ce5f3426d1dace5dd4ce9dd0b878a992
Trieutien2001vn <19130230@st.hcmuaf.edu.vn>
2023-04-05 23:16:23 +0700

    up BangPhanCong

---
+++ QuanLyKhoHang/frontend/src/components/Login/Login.jsx
@@ -0,0 +1,19 @@
+import { Avatar, Button, Checkbox, Container, FormControlLabel, Grid, Link, Paper, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+
+export default function Login() {
+    
+
+    return(
+       <Paper elevation={10} sx={{display:'flex',justifyContent:'space-between',height:'70vh',width:'80vw',backgroundColor:'white'}}>
+        <Grid>
+{'Hello'}
+        </Grid>
+        <Grid>
+{'Hi'}
+        </Grid>
+       </Paper>
+
+    )
+}
